a532a9cba737165c4bb1d584af099724c210a1b1
mohan <mohanreddy0532@gmail.com>
2021-12-28 18:55:53 +0530

    seed

--- seed.groovy
+++ seed.groovy
@@ -1,4 +1,5 @@
- //pipelineJob('frontend') {
+//Start Jenkins...
+// pipelineJob('frontend') {
 //  configure { flowdefinition ->
 //    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
 //      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
