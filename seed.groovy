6707f8b24d22a6dd8b7f4ed89d868ad5f94a06d8
mohan <mohanreddy0532@gmail.com>
2021-12-29 14:56:35 +0530

    jenkins

--- seed.groovy
+++ seed.groovy
@@ -41,15 +41,19 @@
 //    }
 //}
 
-pipeline {
-    agent any
-
-    stages {
-        stage('Ansible Playbook Run') {
-            steps {
-                sh 'ansible-playbook 08-parallel-plays.yml'
-            }
-        }
+job('example') {
+    logRotator(-1, 10)
+    jdk('Java 8')
+    scm {
+        github('jenkinsci/job-dsl-plugin', 'master')
     }
-
+    triggers {
+        githubPush()
+    }
+    steps {
+        gradle('clean build')
+    }
+    publishers {
+        archiveArtifacts('job-dsl-plugin/build/libs/job-dsl.hpi')
+    }
 }
