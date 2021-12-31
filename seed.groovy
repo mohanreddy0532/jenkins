c367b53c00e16abbbdfbee41df27e4c2e9561a6e
mohan <mohanreddy0532@gmail.com>
2021-12-31 14:24:14 +0530

    cart jenkins

--- seed.groovy
+++ seed.groovy
@@ -41,3 +41,29 @@ pipelineJob('roboshop-ansible') {
         }
     }
 }
+
+folder('CI-Pipelines') {
+    displayName('CI-Pipelines')
+    description('CI-Pipelines')
+}
+
+pipelineJob('CI-Pipelines/cart') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/raghudevopsb61/cart.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+                    'lightweight'(true)
+        }
+    }
+}
