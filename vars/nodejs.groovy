9bd277951119aa36f7e1611c053a6a5473410ba7
mohan <mohanreddy0532@gmail.com>
2021-12-31 16:18:50 +0530

    node js for components.

---
+++ vars/nodejs.groovy
@@ -0,0 +1,49 @@
+def call() {
+    pipeline {
+        agent {
+            label "${BUILD_LABEL}"
+        }
+
+        triggers {
+            pollSCM('H/2 * * * *')
+        }
+
+        stages {
+
+
+            stage('Check the Code Quality') {
+                steps {
+                    script {
+                        common.sonarQube()
+                    }
+                }
+            }
+
+            stage('Lint Checks') {
+                steps {
+                    sh 'echo Lint Cases'
+                }
+            }
+
+            stage('Test Cases') {
+                steps {
+                    sh 'echo Test Cases'
+                }
+            }
+
+            stage('Publish Artifacts') {
+                steps {
+                    sh 'echo Publish Artifacts'
+                }
+            }
+
+        }
+
+        post {
+            always {
+                cleanWs()
+            }
+        }
+
+    }
+}
