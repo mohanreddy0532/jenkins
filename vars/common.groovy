53f854cb77b9d387c0ca38a4d0138232e7240b2b
mohan <mohanreddy0532@gmail.com>
2021-12-31 18:48:10 +0530

    sonar qube code test

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,5 +1,5 @@
 def sonarQube() {
-//  sh 'sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.15.251:9000 -Dsonar.login=admin -Dsonar.password=admin123'
-//  sh 'sonar-quality-gate.sh admin admin123 172.31.15.251 ${COMPONENT}'
+  sh 'sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.15.251:9000 -Dsonar.login=admin -Dsonar.password=admin123'
+  sh 'sonar-quality-gate.sh admin admin123 172.31.15.251 ${COMPONENT}'
     println 'SonarQube Testing'
 }
