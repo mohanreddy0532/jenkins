2074998a247001bc1d1d897c277bfc2e837ec5c5
mohan <mohanreddy0532@gmail.com>
2021-12-31 16:46:52 +0530

    java groovy

---
+++ vars/java.groovy
@@ -0,0 +1,48 @@
+def call() {
+    pipeline {
+        agent {
+            label "${BUILD_LABEL}"
+        }
+
+        triggers {
+            pollSCM('H/2 * * * *')
+        }
+
+        stages {
+
+            stage('Compile the Code') {
+                steps {
+                    sh 'mvn compile'
+                }
+            }
+
+            stage('Check the Code Quality') {
+                steps {
+                    script {
+                        common.sonarQube()
+                    }
+                }
+            }
+
+            stage('Lint Checks') {
+                steps {
+                    sh 'echo Test Cases'
+                }
+            }
+
+            stage('Test Cases') {
+                steps {
+                    sh 'echo Test Cases'
+                }
+            }
+
+        }
+
+        post {
+            always {
+                cleanWs()
+            }
+        }
+
+    }
+}
