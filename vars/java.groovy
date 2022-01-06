acc01c886fd72758777af6b017b58429322b8c66
mohan <mohanreddy0532@gmail.com>
2022-01-06 20:12:41 +0530

    java groovy

--- vars/java.groovy
+++ vars/java.groovy
@@ -4,11 +4,26 @@ def call() {
             label "${BUILD_LABEL}"
         }
 
-        triggers {
-            pollSCM('H/2 * * * *')
+//    triggers {
+//      pollSCM('H/2 * * * *')
+//    }
+
+        environment {
+            PROG_LANG_NAME = "java"
+            PROG_LANG_VERSION = "1.8"
+            NEXUS = credentials('NEXUS')
         }
 
         stages {
+
+            stage('Label Builds') {
+                steps {
+                    script {
+                        env.gitTag = GIT_BRANCH.split('/').last()
+                        addShortText background: 'white', borderColor: 'white', color: 'red', link: '', text: "${gitTag}"
+                    }
+                }
+            }
 
             stage('Compile the Code') {
                 steps {
@@ -33,6 +48,18 @@ def call() {
             stage('Test Cases') {
                 steps {
                     sh 'echo Test Cases'
+                }
+            }
+
+            stage('Publish Artifacts') {
+                when {
+                    expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true' ]) }
+                }
+                steps {
+                    script {
+                        common.prepareArtifacts()
+                        common.publishArtifacts()
+                    }
                 }
             }
 
