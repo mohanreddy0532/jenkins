f235ab36ce2828da1b1b5691dac091d8cc95fef2
mohan <mohanreddy0532@gmail.com>
2022-01-18 13:51:18 +0530

    tf groovy

---
+++ vars/terraform.groovy
@@ -0,0 +1,47 @@
+def call() {
+    pipeline {
+        agent {
+            label "${BUILD_LABEL}"
+        }
+
+        //environment {}
+
+        options {
+            ansiColor('xterm')
+        }
+
+        parameters {
+            choice(name: 'ENVIRONMENT', choices: ['', 'dev', 'prod'], description: 'Pick Environment')
+            choice(name: 'ACTION', choices: ['', 'apply', 'destroy'], description: 'Pick Terraform Action')
+        }
+
+        stages {
+
+            stage('Label Builds') {
+                steps {
+                    script {
+                        addShortText background: 'white', borderColor: 'white', color: 'red', link: '', text: "${ENVIRONMENT}"
+                        addShortText background: 'white', borderColor: 'white', color: 'red', link: '', text: "${ACTION}"
+                    }
+                }
+            }
+
+            stage('Apply Terraform Action') {
+                steps {
+                    sh '''
+            terraform init -backend-config=env/${ENVIRONMENT}-backend.tfvars
+            terraform ${ACTION} -auto-approve -var-file=env/${ENVIRONMENT}.tfvars
+          '''
+                }
+            }
+
+        }
+
+        post {
+            always {
+                cleanWs()
+            }
+        }
+
+    }
+}
