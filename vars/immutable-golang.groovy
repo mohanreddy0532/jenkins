f1f99d43e218f75ff836271adf1887c71a6b03ae
mohan <mohanreddy0532@gmail.com>
2022-02-06 20:07:16 +0530

    imm. golang

---
+++ vars/immutable-golang.groovy
@@ -0,0 +1,84 @@
+def call() {
+    pipeline {
+        agent {
+            label "${BUILD_LABEL}"
+        }
+
+//    triggers {
+//      pollSCM('H/2 * * * *')
+//    }
+
+        environment {
+            PROG_LANG_NAME = "golang"
+            PROG_LANG_VERSION = "1.5"
+            NEXUS = credentials('NEXUS')
+        }
+
+        options {
+            ansiColor('xterm')
+        }
+
+        stages {
+
+            stage('Label Builds') {
+                steps {
+                    script {
+                        env.gitTag = GIT_BRANCH.split('/').last()
+                        addShortText background: 'white', borderColor: 'white', color: 'red', link: '', text: "${gitTag}"
+                    }
+                }
+            }
+
+            stage('Check the Code Quality') {
+                steps {
+                    script {
+                        common.sonarQube()
+                    }
+                }
+            }
+
+            stage('Lint Checks') {
+                steps {
+                    sh 'echo Test Cases'
+                }
+            }
+
+            stage('Test Cases') {
+                steps {
+                    sh 'echo Test Cases'
+                }
+            }
+
+            stage('Publish Artifacts') {
+                when {
+                    expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true' ]) }
+                }
+                steps {
+                    script {
+                        common.prepareArtifacts()
+                        common.publishArtifacts()
+                    }
+                }
+            }
+
+            stage('Publish AMI') {
+                when {
+                    expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true' ]) }
+                }
+                steps {
+                    script {
+                        common.makeAMI()
+                    }
+                }
+            }
+
+        }
+
+        post {
+            always {
+                cleanWs()
+            }
+        }
+
+    }
+}
