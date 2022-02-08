1b9e83c696f28ad7a309553fce2fedd3c0967c3e
mohan <mohanreddy0532@gmail.com>
2022-02-08 21:11:38 +0530

    jenkin

---
+++ instance-refresh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ASG=$1
+
+aws autoscaling start-instance-refresh --auto-scaling-group-name ${ASG}
+while true ; do
+  STATUS=$(aws autoscaling describe-instance-refreshes --auto-scaling-group-name ${ASG} | jq .InstanceRefreshes[0].Status | sed -e 's/"//g')
+  echo Instance Refresh Status - ${STATUS}
+  if [ "${STATUS}" == "InProgress" -o "${STATUS}" == "Pending" ]; then
+    sleep 30
+  elif [ "${STATUS}" == "Successful" ]; then
+    exit 0
+  else
+    exit 1
+  fi
+done
