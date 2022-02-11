3e6f2f5b4fcd114bb242993482c4b01f9ca9b050
mohan <mohanreddy0532@gmail.com>
2022-02-11 18:20:37 +0530

    tf rds db

--- instance-refresh.sh
+++ instance-refresh.sh
@@ -2,7 +2,7 @@
 
 ASG=$1
 
-aws autoscaling start-instance-refresh --auto-scaling-group-name ${ASG}
+aws autoscaling start-instance-refresh --auto-scaling-group-name ${ASG} --preferences '{"InstanceWarmup": 180, "MinHealthyPercentage": 0}'
 while true ; do
   STATUS=$(aws autoscaling describe-instance-refreshes --auto-scaling-group-name ${ASG} | jq .InstanceRefreshes[0].Status | sed -e 's/"//g')
   echo Instance Refresh Status - ${STATUS}
