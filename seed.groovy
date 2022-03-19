76d59143193ac1bbc6bc03ff25258a3f8bb77596
mohan <mohanreddy0532@gmail.com>
2022-03-19 13:08:55 +0530

    seeding

--- seed.groovy
+++ seed.groovy
@@ -110,7 +110,7 @@ pipelineJob('Mutable/VPC') {
             'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                 'userRemoteConfigs' {
                     'hudson.plugins.git.UserRemoteConfig' {
-                        'url'('https://github.com/raghudevopsb61/terraform-vpc.git')
+                        'url'('https://github.com/mohanreddy0532/terraform-0322-vpc.git')
                     }
                 }
                 'branches' {
