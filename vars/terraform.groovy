78d50edc66c24c65444d3b0c9509f6b8338207f2
mohan <mohanreddy0532@gmail.com>
2022-03-19 15:14:00 +0530

    terra groovy

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -31,7 +31,7 @@ def call() {
                     sh '''
             terraform init -backend-config=env/${ENVIRONMENT}-backend.tfvars
             terraform ${ACTION} -auto-approve -var-file=env/${ENVIRONMENT}.tfvars
-          '''
+            '''
                 }
             }
 
